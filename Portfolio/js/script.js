e031b698a60ae3ec3f74b40c6dcfef1fc47eea96
KhBulgurov <70327969+KhBulgurov@users.noreply.github.com>
2021-01-22 20:52:55 +0300

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const percent = document.querySelectorAll('.scale__percent'),
+      lines = document.querySelectorAll('.scale__filing');
+
+percent.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML
+});
